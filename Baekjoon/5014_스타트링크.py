c803f913524746938accbc31526f13da95f54747
cheery72 <cheery7272@naver.com>
2020-10-27 15:53:39 +0900

    Algorithm

---
+++ Baekjoon/5014_스타트링크.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+f, s, g, u, d = map(int, input().split())
+
+dx=[u,d]
+
+def bfs():
+    q = deque([s])
+    visited= [0]*(f+5)
+    visited[s]=1
+    # q가 비었을 때는 use the stairs를 출력해야함
+    while q:
+        v = q.popleft()
+        # 원하는 층에 도착했을 때 방문한 횟수 출력
+        if v==g:
+            return visited[g]-1
+
+        if 1<=v+u<=f and not visited[v+u]:
+            visited[v+u]= visited[v]+1
+            q.append(v+u)
+        # v-d>0 인 이유는 
+        if  v-d>0 and not visited[v-d]:
+            visited[v-d]= visited[v]+1
+            q.append(v-d)
+
+    return 'use the stairs'
+
+print(bfs())
