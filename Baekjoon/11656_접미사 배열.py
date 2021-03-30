8687b7f01f0fb9455e8e76b2279e4ae39415f927
cheery72 <cheery7272@naver.com>
2021-03-30 23:13:55 +0900

    Algorithm

---
+++ Baekjoon/11656_접미사 배열.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+s = input()
+result = []
+
+for i in range(len(s)-1):
+    result.append(s[i:-1])
+result.sort()
+
+print('\n'.join(result))
+
+# baekjoon
+
+# aekjoon
+# baekjoon
+# ekjoon
+# joon
+# kjoon
+# n
+# on
+# oon
