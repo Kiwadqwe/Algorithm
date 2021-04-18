aa9874175cc080aaeaaf887531806074ccece85a
cheery72 <cheery7272@naver.com>
2021-04-18 21:21:34 +0900

    Algorithm

---
+++ Baekjoon/1269_대칭 차집합.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+a = set(map(int,input().split()))
+b = set(map(int,input().split()))
+
+print(len(a^b))
+
+# 3 5
+# 1 2 4
+# 2 3 4 5 6
+
+# 4
