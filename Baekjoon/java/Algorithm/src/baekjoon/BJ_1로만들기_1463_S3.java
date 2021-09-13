5aa7f616ad337e48090d8153240bba5f85494ab4
cheery72 <cheery7272@naver.com>
2021-09-13 20:34:36 +0900

    Algorithm

--- Baekjoon/java/Algorithm/src/baekjoon/BJ_1로만들기_1463_S3.java
+++ Baekjoon/java/Algorithm/src/baekjoon/BJ_1로만들기_1463_S3.java
@@ -3,35 +3,29 @@ package baekjoon;
 import java.io.*;
 
 public class BJ_1로만들기_1463_S3 {
-	static int x;
-	static int n[];
+	static int N;
+	static int memo[];
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		x = Integer.parseInt(br.readLine());
-		n = new int[x+1];
+		N = Integer.parseInt(br.readLine());
+		memo = new int[N+1];
 		dp();
-		print();
+		System.out.println(memo[N]);
 	}
 
-	private static void print() {
-		System.out.println(n[x]);
-//		for (int i = 0; i < n.length; i++) {
-//			System.out.println(n[i]);
-//		}
-		}
-
 	private static void dp() {
-		for(int i = 2; i<x+1; i++) {
-			n[i] = n[i-1]+1;
-			if (i%3 == 0) {
-				n[i] = Math.min(n[i/3]+1, n[i]);
+		for(int i = 2; i < N+1; i++) {
+			// 1을 빼는 경우
+			memo[i] = memo[i-1]+1;
+			
+			if(i % 3 == 0) {
+				memo[i] = Math.min(memo[i/3]+1, memo[i]);
 			}
-			if (i%2 == 0) {
-				n[i] = Math.min(n[i/2]+1, n[i]);
+			if(i % 2 == 0) {
+				memo[i] = Math.min(memo[i/2]+1, memo[i]);
 			}
-				
 		}
 	}
 }
