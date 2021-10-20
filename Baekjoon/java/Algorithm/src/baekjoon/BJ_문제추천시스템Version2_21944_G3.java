97eb46f64199e29ab9f1a45c2c7b8ac873d9144f
cheery72 <cheery7272@naver.com>
2021-10-21 00:59:22 +0900

    Algorithm

--- Baekjoon/java/Algorithm/src/baekjoon/BJ_문제추천시스템Version2_21944_G3.java
+++ Baekjoon/java/Algorithm/src/baekjoon/BJ_문제추천시스템Version2_21944_G3.java
@@ -1,12 +1,7 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
+import java.io.*;
+import java.util.*;
 
 public class BJ_문제추천시스템Version2_21944_G3 {
 	static int N,M,P,L,G,X;
