db241f3abccd6060049d6c7f50e30d19b3d3af52
cheery72 <cheery7272@naver.com>
2022-06-25 21:06:55 +0900

    Algorithm

---
+++ Baekjoon/python_r/BJ_회문_17609_S1.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+result = []
+def check2(S,start,end):
+    while start<end:
+        if S[start] == S[end]:
+            start+=1
+            end-=1
+        else: return False
+    return True
+
+def check(S,start,end):
+    while start<end:
+        if S[start] == S[end]:
+            start+=1
+            end-=1
+        else:
+            if check2(S,start+1,end) or check2(S,start,end-1):
+                return 1
+            else:
+                return 2
+    return 0
+
+for i in range(T):
+    S = input().strip()
+
+    result.append(check(S,0,len(S)-1))
+
+for r in result:
+    print(r)
