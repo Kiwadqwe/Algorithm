e73de42c5bf7f375c23d09f26ed312a27b4158d2
cheery72 <cheery7272@naver.com>
2022-10-27 20:47:17 +0900

    Algorithm

---
+++ Baekjoon/python/BJ_용액_2467_G5.py
@@ -0,0 +1,28 @@
+import sys,time
+input = sys.stdin.readline
+
+N = int(input())
+array = list(map(int,input().split()))
+
+s,e,near = 0,N-1,2147483647
+result_s,result_e = 0,0
+
+while s < e:
+    hap = abs(array[s]+array[e])
+    
+    if hap < near:
+        result_s = s
+        result_e = e
+        near = hap
+        
+        if abs(array[s]+array[e-1]) < abs(array[s+1]+array[e]):
+            e-=1
+        else:
+            s+=1
+    else:
+        if abs(array[s]+array[e-1]) < abs(array[s+1]+array[e]):
+            e-=1
+        else:
+            s+=1
+        
+print(array[result_s],array[result_e])
