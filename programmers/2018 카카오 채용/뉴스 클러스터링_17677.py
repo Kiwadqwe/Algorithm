aa3c12f77dd7e1f536978e0fe6b531b9bee15384
cheery72 <cheery7272@naver.com>
2022-11-12 21:06:06 +0900

    Algorithm

--- programmers/2018 카카오 채용/뉴스 클러스터링_17677.py
+++ programmers/2018 카카오 채용/뉴스 클러스터링_17677.py
@@ -24,7 +24,7 @@ def solution(str1, str2):
     else:
         return int(len(iters)/len(uin) * 65536)
 
-# print(solution("FRANCE", "french"))
-# print(solution("handshake", "shake hands"))
+print(solution("FRANCE", "french"))
+print(solution("handshake", "shake hands"))
 print(solution("aa1+aa2", "AAAA12"))
 # print(solution("E=M*C^2", "e=m*c^2"))
