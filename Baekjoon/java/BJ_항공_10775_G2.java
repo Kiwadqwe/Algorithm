3c87eb2a94f0acc57d1d744f4ca7f2e2350682e4
cheery72 <cheery7272@naver.com>
2022-12-10 21:15:34 +0900

    Algorithm

--- Baekjoon/java/BJ_항공_10775_G2.java
+++ Baekjoon/java/BJ_항공_10775_G2.java
@@ -1,3 +1,8 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,32 +16,30 @@ public class BJ_항공_10775_G2 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        G = Integer.parseInt((new StringTokenizer(br.readLine())).nextToken());
+        P = Integer.parseInt((new StringTokenizer(br.readLine())).nextToken());
+        p = new int[G + 1];
 
-        G = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
-        P = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
-        p = new int[G+1];
-
-        for (int i = 0; i < G+1; i++) {
-            p[i] = i;
+        int i;
+        for(i = 0; i < G + 1; p[i] = i++) {
         }
 
-        for(int i = 0; i < P; i++){
-            int A = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
+        for(i = 0; i < P; ++i) {
+            int A = Integer.parseInt((new StringTokenizer(br.readLine())).nextToken());
             int f = find(A);
+            if (f == 0) {
+                break;
+            }
 
-            if(f == 0) break;
-
-            p[f] = p[f-1];
-
-            result++;
+            p[f] = p[f - 1];
+            ++result;
         }
 
         System.out.println(result);
-
     }
 
-    public static int find(int x){
-        if(x != p[x]){
+    public static int find(int x) {
+        if (x != p[x]) {
             p[x] = find(p[x]);
         }
 
