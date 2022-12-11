c6585c873be188bac98dcbb9b0cda424fd5aac62
cheery72 <cheery7272@naver.com>
2022-12-11 20:19:42 +0900

    Algorithm

---
+++ Baekjoon/에라토스테네스의 체.py
@@ -0,0 +1,16 @@
+import math
+
+N = 1000
+array = [True for i in range(N+1)]
+
+for i in range(2,int(math.sqrt(N))+1):
+    if array[i]:
+        j = 2
+        while i * j <= N:
+            array[i*j] = False
+            j+=1    
+
+for i in range(2,N+1):
+    if array[i]:
+        print(i,end =' ')
+print()
