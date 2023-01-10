e04932185a049f60046b8e80491c0faf3c408d8e
Kiwadqwe <mrmr0236@naver.com>
2023-01-10 19:15:00 +0900

    a

--- Baekjoon/에라토스테네스의 체.py
+++ Baekjoon/에라토스테네스의 체.py
@@ -5,7 +5,6 @@ array = [True for i in range(N+1)]
 
 for i in range(2,int(math.sqrt(N))+1):
     if array[i]:
-        k = 3
         j = 2
         while i * j <= N:
             array[i*j] = False
